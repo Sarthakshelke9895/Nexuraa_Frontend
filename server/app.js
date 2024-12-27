56d0c4243bac74d18a14b22942fbaa8874a9f9f3
Dell <sarthak shelke>
2024-12-27 14:02:21 +0530

    Cors rigin Email is Send from Desktop but not from mobile Applications

--- server/app.js
+++ server/app.js
@@ -4,7 +4,15 @@ const cors = require("cors");
 const app = express();
 const port = 5000;
 
-app.use(cors());
+
+
+var corsOptions = {
+  origin: 'https://nexuraa.netlify.app/',
+  methods:"GET,POST,PUT,DELETE,PATCH,HEAD",
+  credentials:true,
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+app.use(cors(corsOptions));
 
 app.use(express.json({ limit: "25mb" }));
 app.use(express.urlencoded({ limit: "25mb" }));
