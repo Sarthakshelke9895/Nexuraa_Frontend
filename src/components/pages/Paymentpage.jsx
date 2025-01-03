f7aaf1b3504b2be71c99cb5ffa98c4416d32dd00
Dell <sarthak shelke>
2025-01-04 01:04:11 +0530

    email Format updated2

--- src/components/pages/Paymentpage.jsx
+++ src/components/pages/Paymentpage.jsx
@@ -36,7 +36,7 @@ const Paymentpage = () => {
         <div className="payment-container">
       <h1 className="payment-header">Upload Your App</h1>
       <p className="payment-subtext">
-        Pay <strong>21.00</strong> INR to get your app listed on our platform.<br></br>Also Check the Email for Further Proceedings. <br></br>If E-mail does not pop up kindly check your spam folder. 
+        Pay <strong>21.00</strong> INR to get your app listed on our platform.Also Check the Email for Further Proceedings.If E-mail does not pop up kindly check your spam folder. 
       </p>
       <button onClick={handleRedirect} className="pay-button">
         Pay <strong>21.00</strong> INR
